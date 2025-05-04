fbe4cc1bdc43be7584bc770314eab8567d78e3d4
gandan <188059221+gandandev@users.noreply.github.com>
2025-05-04 17:27:01 +0900

    fix(HoldToDelete): stop clip-path animation on pointer leave

--- src/lib/experiments/HoldToDelete.svelte
+++ src/lib/experiments/HoldToDelete.svelte
@@ -37,7 +37,10 @@
 >
   <div
     aria-hidden="true"
-    class="absolute flex size-5 h-full w-full items-center justify-center gap-2 rounded-full bg-red-100 text-red-500 duration-200 [clip-path:inset(0_100%_0_0)] group-active:duration-1000 group-active:ease-linear group-active:[clip-path:inset(0_0_0_0)]"
+    class="absolute flex size-5 h-full w-full items-center justify-center gap-2 rounded-full bg-red-100 text-red-500 duration-200 [clip-path:inset(0_100%_0_0)]"
+    class:duration-1000={isHolding}
+    class:ease-linear={isHolding}
+    class:![clip-path:inset(0_0_0_0)]={isHolding}
   >
     <Trash class="size-5" />
     Hold to Delete
