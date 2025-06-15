b926d6bb9f58eefa36fde0d34182534d2e9dc9a0
gandan <188059221+gandandev@users.noreply.github.com>
2025-06-15 13:16:02 +0900

    feat(SpaceDatetime): show canvas on click

--- src/lib/experiments/SpaceDatetime.svelte
+++ src/lib/experiments/SpaceDatetime.svelte
@@ -1,19 +1,34 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { Calendar } from '@lucide/svelte'
 
   let selectedDate = $state(new Date())
   let isOpen = $state(false)
+
+  let canvas: HTMLCanvasElement | null = $state(null)
+
+  onMount(() => {
+    if (!canvas) return
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+  })
 </script>
 
-<button
-  class="transition-300 gap-2Z flex cursor-pointer items-center rounded-xl bg-neutral-100 px-3 py-2"
-  onclick={() => (isOpen = true)}
->
-  <Calendar class="h-4 w-4" />
-  <span
-    >{selectedDate.toLocaleDateString(undefined, { month: 'short', day: 'numeric' })}, {selectedDate.toLocaleTimeString(
-      undefined,
-      { hour: 'numeric', minute: '2-digit' }
-    )}</span
+{#if isOpen}
+  <div class="relative h-full w-full bg-black">
+    <canvas bind:this={canvas} class="h-full w-full"></canvas>
+  </div>
+{:else}
+  <button
+    class="transition-300 gap-2Z flex cursor-pointer items-center rounded-xl bg-neutral-100 px-3 py-2"
+    onclick={() => (isOpen = true)}
   >
-</button>
+    <Calendar class="size-5" />
+    <span>
+      {selectedDate.toLocaleDateString(undefined, { month: 'short', day: 'numeric' })}, {selectedDate.toLocaleTimeString(
+        undefined,
+        { hour: 'numeric', minute: '2-digit' }
+      )}
+    </span>
+  </button>
+{/if}
