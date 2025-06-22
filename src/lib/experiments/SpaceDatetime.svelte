6bae7a5b42dc5ec66c2fe6f7b3eb439cf1895c1b
gandan <188059221+gandandev@users.noreply.github.com>
2025-06-22 09:32:33 +0900

    feat(SpaceDatetime): transition between button and canvas

--- src/lib/experiments/SpaceDatetime.svelte
+++ src/lib/experiments/SpaceDatetime.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import { Calendar } from '@lucide/svelte'
+  import { blur } from 'svelte/transition'
+  import { versatile } from '../transitions'
 
   let selectedDate = $state(new Date())
   let isOpen = $state(false)
@@ -14,21 +16,87 @@
   })
 </script>
 
-{#if isOpen}
-  <div class="relative h-full w-full bg-black">
+<button
+  class="flex justify-center rounded-xl"
+  class:mixed-transitions-open={isOpen}
+  class:mixed-transitions-closed={!isOpen}
+  class:h-10={!isOpen}
+  class:w-42={!isOpen}
+  class:size-full={isOpen}
+  class:bg-neutral-100={!isOpen}
+  class:bg-black={isOpen}
+  class:cursor-pointer={!isOpen}
+  onclick={() => (isOpen = !isOpen)}
+>
+  {#if isOpen}
     <canvas bind:this={canvas} class="h-full w-full"></canvas>
-  </div>
-{:else}
-  <button
-    class="transition-300 flex cursor-pointer items-center gap-2 rounded-xl bg-neutral-100 px-3 py-2"
-    onclick={() => (isOpen = true)}
-  >
-    <Calendar class="size-5" />
-    <span>
-      {selectedDate.toLocaleDateString(undefined, { month: 'short', day: 'numeric' })}, {selectedDate.toLocaleTimeString(
-        undefined,
-        { hour: 'numeric', minute: '2-digit' }
-      )}
-    </span>
-  </button>
-{/if}
+  {:else}
+    <div
+      class="absolute inset-0 mx-auto flex w-42 items-center justify-evenly"
+      class:starting:w-42={!isOpen}
+      in:versatile={{
+        blur: {
+          duration: 300,
+          from: 10,
+          to: 0
+        },
+        scale: {
+          duration: 300,
+          from: 1.2,
+          to: 1
+        },
+        opacity: {
+          duration: 300,
+          from: 0,
+          to: 1
+        }
+      }}
+      out:versatile={{
+        blur: {
+          duration: 300,
+          from: 10,
+          to: 0
+        },
+        scale: {
+          duration: 300,
+          from: 1.2,
+          to: 1
+        },
+        opacity: {
+          duration: 200,
+          from: 0,
+          to: 1
+        }
+      }}
+    >
+      <Calendar class="size-5" />
+      <span>
+        {selectedDate.toLocaleDateString(undefined, { month: 'short', day: 'numeric' })}, {selectedDate.toLocaleTimeString(
+          undefined,
+          { hour: 'numeric', minute: '2-digit' }
+        )}
+      </span>
+    </div>
+  {/if}
+</button>
+
+<style>
+  .mixed-transitions-open {
+    --ease: cubic-bezier(0.5, 0, 0.3, 1);
+
+    transition:
+      width 300ms var(--ease),
+      height 300ms var(--ease),
+      background-color 500ms ease,
+      cursor 500ms ease;
+  }
+
+  .mixed-transitions-closed {
+    --ease-back-out-small: cubic-bezier(0.4, 1.2, 0.5, 1);
+    transition:
+      width 500ms var(--ease-back-out-small),
+      height 500ms var(--ease-back-out-small),
+      background-color 300ms ease,
+      cursor 500ms ease;
+  }
+</style>
